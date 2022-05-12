4ea506ef102c056bcab0c0adc425fcb63b7a208c
Kate Maher <katemaher00@gmail.com>
2022-05-12 22:53:01 +0100

    Ammend file error

---
+++ profiles/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def profiles(request):
+    """ Display the user's profile """
+    template = 'profiles/profiles.html'
+    context = {}
+
+    return render(request, template, context)
