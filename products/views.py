8ae81f3b62f775a1b5fd5a7c90c70705f7b11d2b
Kate Maher <katemaher00@gmail.com>
2022-05-15 16:20:19 +0100

    Add delete products functionality

--- products/views.py
+++ products/views.py
@@ -75,19 +75,24 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ Add a product to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Product added successfully!')
-            return redirect(reverse('add_product'))
+            product = form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to add product. Please ensure form is valid.')
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
         form = ProductForm()
+        
     template = 'products/add_product.html'
     context = {
-        'form' : form,
+        'form': form,
     }
 
     return render(request, template, context)
@@ -96,7 +101,7 @@ def edit_product(request, product_id):
     """ Edit a product in the store """
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
-        from = ProductForm(request.POST, request.FILES, instance=product)
+        form = ProductForm(request.POST, request.FILES, instance=product)
         if form.is_valid():
             form.save()
             messages.success(request, 'Product successfully updated!')
@@ -113,6 +118,13 @@ def edit_product(request, product_id):
         'product' : product,
     }
     return render(request, template, context)
+
+def delete_product(request, product_id):
+    """ Delete a product in the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted successfully!')
+    return redirect(reverse('products'))
 
 
 class AddReviewView(CreateView):
