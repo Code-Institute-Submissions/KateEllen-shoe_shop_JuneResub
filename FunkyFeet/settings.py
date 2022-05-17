1dcb514919ff427442f27541df38ea59aa463383
Kate Maher <katemaher00@gmail.com>
2022-05-17 22:43:45 +0100

    Change DEBUG to development

--- FunkyFeet/settings.py
+++ FunkyFeet/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['funky-feet.herokuapp.com', 'localhost', '127.0.0.1']
 
