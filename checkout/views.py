8ac1346f2f70b099c9deb38aab5178d00ac8a306
Kate Maher <katemaher00@gmail.com>
2022-05-20 18:51:21 +0100

    Ammend checkout.html client_secret to match what is in stripe_elements.js

--- checkout/views.py
+++ checkout/views.py
@@ -19,7 +19,7 @@ import json
 def cache_checkout_data(request):
     try:
         pid = request.POST.get('client_secret').split('_secret')[0]
-        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.api_key = settings.STRIPE_SECRET_KEY 
         stripe.PaymentIntent.modify(pid, metadata={
             'bag': json.dumps(request.session.get('bag', {})),
             'save_info': request.POST.get('save_info'),
@@ -27,8 +27,7 @@ def cache_checkout_data(request):
         })
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(request, 'Sorry, your payment cannot be \
-            processed right now. Please try again later.')
+        messages.error(print(f"CACHE ERROR: {e}"))
         return HttpResponse(content=e, status=400)
 
 
