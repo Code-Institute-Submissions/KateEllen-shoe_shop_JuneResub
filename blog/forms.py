b3cbbb04f833fa9b893f0f76a6a94ce4e3e7d8e1
Kate Maher <katemaher00@gmail.com>
2022-06-22 20:58:01 +0100

    Migration for blog

--- blog/forms.py
+++ blog/forms.py
@@ -5,10 +5,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = (
-            'title',
-            'product',
-            'slug',
-            'author',
-            'body',
+            'post_title',
+            'post_content',
         )
 
--- blog/models.py
+++ blog/models.py
@@ -1,26 +1,22 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
-
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    product = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
-    slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
-    body = models.TextField()
+    post_title = models.CharField(max_length=100)
+    post_content = models.TextField()
+    post_date_posted = models.DateTimeField(default=timezone.now)
+    post_author = models.ForeignKey(User, default="", blank=True,
+                                    on_delete=models.CASCADE,
+                                    related_name='post')
 
     class Meta:
-        ordering = ["-created_on"]
+        ordering = ['-post_date_posted']
 
     def __str__(self):
         return self.post_title
-
 
 class Comment(models.Model):
     post = models.ForeignKey(
