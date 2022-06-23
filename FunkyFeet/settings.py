115fac4048dacb9952f031ccd96707a6915040f1
Kate Maher <katemaher00@gmail.com>
2022-06-23 23:25:51 +0100

    Deployment commit

--- FunkyFeet/settings.py
+++ FunkyFeet/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = ['funky-feet.herokuapp.com', 'localhost', '127.0.0.1']
 
